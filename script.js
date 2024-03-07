db270c749bd18e7a49dbb9f65b744b08cb543e52
Ela <radisza5@gmail.com>
2024-03-07 11:52:50 +0100

    Add button to change number of pixels on board

--- script.js
+++ script.js
@@ -14,9 +14,6 @@ function new_board(max_board_size, pixels_per_row) {
     board.innerHTML = '';
     set_square_size(board, new_board_size);
 
-    console.log(new_board_size);
-    console.log(pixel_size);
-    
     for (let i = 0; i < pixels_per_row*pixels_per_row; i++) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
@@ -30,3 +27,24 @@ function new_board(max_board_size, pixels_per_row) {
 
 new_board(BOARD_SIZE, DEFAULT_PIXELS_PER_ROW);
 
+let new_board_btn = document.querySelector(".new_board");
+new_board_btn.addEventListener('click', () => {
+    let user_input = prompt("Give number of pixels per row between 1-100.");
+    if (user_input == undefined) {
+        alert(`Undefined input. Expected integer between 1-100.`);
+        return;
+    }
+
+    let pixels_per_row = parseInt(user_input);
+    if (isNaN(pixels_per_row)) {
+        alert(`Invalid input. Expected integer between 1-100.`);
+        return;
+    }
+
+    if (pixels_per_row < 1 || pixels_per_row > 100) {
+        alert(`Wrong number. Expected integer between 1-100.`);
+        return;
+    }
+
+    new_board(BOARD_SIZE, pixels_per_row);
+})
