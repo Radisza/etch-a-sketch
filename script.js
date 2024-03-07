b746869f70cc55de0c205efcb37f6cb97dcde514
Ela <radisza5@gmail.com>
2024-03-07 15:51:46 +0100

    Change hover effect to have random color instead of red

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function set_square_size(element, size) {
     element.style.height = size + 'px';
 }
 
-function new_board(max_board_size, pixels_per_row) {
+function create_new_board(max_board_size, pixels_per_row) {
     let pixel_size = Math.floor(max_board_size / pixels_per_row);
     let new_board_size = pixels_per_row*pixel_size;
     
@@ -18,12 +18,30 @@ function new_board(max_board_size, pixels_per_row) {
         let pixel = document.createElement('div');
         pixel.classList.add('pixel');
         set_square_size(pixel, pixel_size);
+        set_hover_effect(pixel);
 
         board.appendChild(pixel);
     }
 }
 
-new_board(BOARD_SIZE, DEFAULT_PIXELS_PER_ROW);
+function get_random_int(max) {
+    return Math.floor(Math.random(max) * max);
+}
+
+function set_background_color(element) {
+        let random = () => { return get_random_int(255); };
+        let random_color = `rgb(${random()}, ${random()}, ${random()})`;
+        element.style.backgroundColor=random_color;
+}
+
+function unset_background_color(element) {
+    element.style.backgroundColor='';
+}
+
+function set_hover_effect(pixel) {
+    pixel.addEventListener('mouseover', (event) => set_background_color(event.target));
+    pixel.addEventListener('mouseout', (event) => unset_background_color(event.target));
+}
 
 let new_board_btn = document.querySelector(".new_board");
 new_board_btn.addEventListener('click', () => {
@@ -34,15 +52,13 @@ new_board_btn.addEventListener('click', () => {
     }
 
     let pixels_per_row = parseInt(user_input);
-    if (isNaN(pixels_per_row)) {
+    if (isNaN(pixels_per_row) || pixels_per_row < 1 || pixels_per_row > 100) {
         alert(`Invalid input. Expected integer between 1-100.`);
         return;
     }
 
-    if (pixels_per_row < 1 || pixels_per_row > 100) {
-        alert(`Wrong number. Expected integer between 1-100.`);
-        return;
-    }
-
-    new_board(BOARD_SIZE, pixels_per_row);
+    create_new_board(BOARD_SIZE, pixels_per_row);
 })
+
+
+create_new_board(BOARD_SIZE, DEFAULT_PIXELS_PER_ROW);
