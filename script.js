9125a1b524072c6950f498d9e2c4d18d51ada9d3
Ela <radisza5@gmail.com>
2024-03-07 11:18:13 +0100

    Change new_board input argument to accept pixels per row
    Replace `pixel_size` argument to `pixels per row`

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+const BOARD_SIZE = 960;
+const DEFAULT_PIXELS_PER_ROW = 30;
+
 function set_square_size(element, size) {
     element.style.width = size + 'px';
     element.style.height = size + 'px';
 }
 
-function new_board(max_board_size, pixel_size) {
-    let pixels_per_row = Math.floor(max_board_size / pixel_size);
+function new_board(max_board_size, pixels_per_row) {
+    let pixel_size = Math.floor(max_board_size / pixels_per_row);
     let new_board_size = pixels_per_row*pixel_size;
     
     let board = document.querySelector('.board');
@@ -25,5 +28,5 @@ function new_board(max_board_size, pixel_size) {
     }
 }
 
+new_board(BOARD_SIZE, DEFAULT_PIXELS_PER_ROW);
 
-new_board(960, 32);
